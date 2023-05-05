0b10300d9c5968b08591f876cf345a521bbb5204
Dolbyk_Digital <denusveluchko2017@gmail.com>
2023-05-05 23:58:12 +0300

    update

---
+++ calorie-tracker/src/main/java/com/calorie/calorie/tracker/controllers/ProController.java
@@ -0,0 +1,35 @@
+package com.calorie.calorie.tracker.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/index")
+    public String showRegPage() {
+        return "index";
+    }
+
+    @GetMapping("/fscreen")
+    public String showMenu() {
+        return "fscreen";
+    }
+
+    @GetMapping("/faq")
+    public String showFaqs() {
+        return "faq";
+    }
+
+    @GetMapping("/app")
+    public String showJs() {
+        return "app";
+    }
+
+
+}
---
+++ calorie-tracker/src/test/java/com/calorie/calorie/tracker/CalorieTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calorie.calorie.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalorieTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
