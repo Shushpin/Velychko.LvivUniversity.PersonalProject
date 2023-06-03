81574a36784ba4bd1f1dba168dbba4ff933c0f61
Dolbyk_Digital <denusveluchko2017@gmail.com>
2023-06-03 21:11:45 +0300

    reset

--- calorie-tracker/src/main/java/com/calorie/calorie/tracker/dao/RoleDao.java
+++
@@ -1,7 +0,0 @@
-package com.calorie.calorie.tracker.dao;
-
-import com.calorie.calorie.tracker.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleDao extends JpaRepository<Role, Long> {
-}
--- calorie-tracker/src/main/java/com/calorie/calorie/tracker/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.calorie.calorie.tracker.dao;
-
-import com.calorie.calorie.tracker.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
